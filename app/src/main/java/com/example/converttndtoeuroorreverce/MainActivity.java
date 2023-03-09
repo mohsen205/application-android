4a4519c5d9f664b4d2ea879f2ed12b9737891c6a
mohsen205 <mohsenmnassri7@gmail.com>
2023-03-09 12:02:16 +0100

    first commit

---
+++ app/src/main/java/com/example/converttndtoeuroorreverce/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.converttndtoeuroorreverce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText amount = findViewById(R.id.amount);
+        Button btn = findViewById(R.id.btn);
+        RadioButton  euroToTnd = findViewById(R.id.myRadioButton1);
+        TextView resulat = findViewById(R.id.myTextView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double a = Double.parseDouble(amount.getText().toString().trim());
+                if(euroToTnd.isChecked()){
+                    double result = a * 3.6;
+                    resulat.setText(String.format("Resulat : %.2f TND = %.2f EUR", amount));
+                } else {
+                    double result = a * 1.8;
+                    resulat.setText(String.format("Resulat : %.2f TND = %.2f EURO ", amount));
+                }
+            }
+        });
+
+    }
+}
